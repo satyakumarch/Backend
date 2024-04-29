62b30db8062e9245f29513d866854c185635e3bc
Satya Kumar Chaudhary <satyakumarchaudhary603@gmail.com>
2024-04-29 12:26:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app=express();
 dotenv.config({path:"./config/config.env"});
 
 app.use(cors({
-    origin:process.env.FRONTEND_URI,
+    origin:"https://registationsatya.netlify.app/",
     // methods:["POST"],
     credentials:true,
 }))
